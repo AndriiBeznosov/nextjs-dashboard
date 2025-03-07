5c1f82ac34f487721198beaa27823b18033c1229
Andrii Beznosov <99024313+AndriiBeznosov@users.noreply.github.com>
2025-03-07 18:01:52 +0200

    nextjs app course finish

--- app/page.tsx
+++ app/page.tsx
@@ -4,6 +4,8 @@ import Link from "next/link";
 import styles from "@/app/ui/home.module.css";
 import Image from "next/image";
 // import { lusitana } from "./ui/fonts";
+import heroDesktop from "../public/hero-desktop.png";
+import heroMobile from "../public/hero-mobile.png";
 
 export default function Page() {
     return (
@@ -32,14 +34,14 @@ export default function Page() {
                 <div className='flex items-center justify-center p-6 md:w-3/5 md:px-28 md:py-12'>
                     {/* Add Hero Images Here */}
                     <Image
-                        src='/hero-desktop.png'
+                        src={heroDesktop}
                         width={1000}
                         height={760}
                         className='hidden md:block'
                         alt='Screenshots of the dashboard project showing desktop version'
                     />
                     <Image
-                        src='/hero-mobile.png'
+                        src={heroMobile}
                         width={560}
                         height={620}
                         className='block md:hidden'
